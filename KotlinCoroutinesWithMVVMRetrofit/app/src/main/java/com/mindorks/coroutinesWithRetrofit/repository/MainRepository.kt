8e4dbf2fdcbf3c3ba8acd47db0c7bfa645946b75
Himanshu Singh <hello2himanshusingh@gmail.com>
2020-01-20 18:34:42 +0530

    Refactored Project

--- KotlinCoroutinesWithMVVMRetrofit/app/src/main/java/com/mindorks/coroutinesWithRetrofit/repository/MainRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.mindorks.coroutinesWithRetrofit.repository
-
-import com.mindorks.coroutinesWithRetrofit.api.RetrofitBuilder
-
-class MainRepository {
-
-    suspend fun getAPIData() = RetrofitBuilder.apiService.fetchDataFromAPI().await()
-
-}
