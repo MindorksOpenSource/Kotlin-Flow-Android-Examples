10929d408d624ef9f5e8ab14893359cd44456eed
Raghunandan Kavi <raghunandan2005@gmail.com>
2021-01-15 12:30:40 +0530

    Use flow as the type instead of using flow builder.

--- app/src/main/java/com/mindorks/kotlinFlow/data/local/DatabaseHelperImpl.kt
+++ app/src/main/java/com/mindorks/kotlinFlow/data/local/DatabaseHelperImpl.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.flow
 class DatabaseHelperImpl(private val appDatabase: AppDatabase) : DatabaseHelper {
 
     override fun getUsers(): Flow<List<User>> =
-        flow { emit(appDatabase.userDao().getAll()) }
+       appDatabase.userDao().getAll()
 
     override fun insertAll(users: List<User>): Flow<Unit> = flow {
         appDatabase.userDao().insertAll(users)
--- app/src/main/java/com/mindorks/kotlinFlow/data/local/dao/UserDao.kt
+++ app/src/main/java/com/mindorks/kotlinFlow/data/local/dao/UserDao.kt
@@ -5,12 +5,13 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import com.mindorks.kotlinFlow.data.local.entity.User
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
 
     @Query("SELECT * FROM user")
-    fun getAll(): List<User>
+    fun getAll(): Flow<List<User>>
 
     @Insert
     fun insertAll(users: List<User>)
